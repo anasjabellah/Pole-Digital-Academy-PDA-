4aefb837ea8e0230c938b72e4bf632418d942a75
anasjabellah <jablcodiing@gmail.com>
2022-10-31 11:07:44 +0100

    edit entities and add class user and set and get

--- src/main/java/com/example/digital_academy_pda/Entities/User.java
+++ src/main/java/com/example/digital_academy_pda/Entities/User.java
@@ -1,4 +1,48 @@
 package com.example.digital_academy_pda.Entities;
 
-public interface User {
+public class User {
+
+    private String nom ;
+    private String prenom ;
+    private String email ;
+    private String telephone ;
+
+    public User(String nom, String prenom, String email, String telephone) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+        this.telephone = telephone;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
 }
