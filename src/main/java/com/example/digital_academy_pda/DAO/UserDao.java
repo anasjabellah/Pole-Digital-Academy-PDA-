df5fef691562a7c4270feda86369aa6803f5fcc1
anasjabellah <jablcodiing@gmail.com>
2022-10-31 11:01:36 +0100

    add PK Dao and REPOSITORY and servlet and services

---
+++ src/main/java/com/example/digital_academy_pda/DAO/UserDao.java
@@ -0,0 +1,4 @@
+package com.example.digital_academy_pda.DAO;
+
+public interface UserDao {
+}
---
+++ src/main/java/com/example/digital_academy_pda/Repository/Implimentation/UserRepositoryImplimentation.java
@@ -0,0 +1,9 @@
+package com.example.digital_academy_pda.Repository.Implimentation;
+
+import com.example.digital_academy_pda.Repository.UserRepository;
+
+public class UserRepositoryImplimentation implements UserRepository {
+
+
+
+}
