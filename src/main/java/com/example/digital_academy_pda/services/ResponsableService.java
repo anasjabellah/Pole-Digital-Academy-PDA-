33d3c21f6472811abf36ca6920484212a204dae5
anasjabellah <jablcodiing@gmail.com>
2022-11-04 09:37:22 +0100

    add respon

--- src/main/java/com/example/digital_academy_pda/services/ResponsableService.java
+++ src/main/java/com/example/digital_academy_pda/services/ResponsableService.java
@@ -1,4 +1,15 @@
 package com.example.digital_academy_pda.services;
 
+import com.example.digital_academy_pda.Entities.Responsable;
+
+import java.util.List;
+
 public interface ResponsableService {
+
+    public void  Add();
+    public List<Responsable> listResponsable();
+    public List<Responsable> searchResponsable(String search);
+    public Responsable findByEmail(String email);
+    public void updateResponsable(long idResponsable);
+    public void remmoveResponsable(long idResponsable);
 }
